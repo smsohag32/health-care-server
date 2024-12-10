7544f24fabfc77f1333ea1d3c94b798a73a27396
smsohag32 <sohagsheik32@gmail.com>
2024-12-10 19:12:35 +0600

    user api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signIn, singUp } from "../controllers/userController.js";
+import { getAllUsers, signIn, singUp } from "../controllers/userController.js";
 
 const router = Router();
 
 router.post("/auth/singin", signIn);
 router.post("/auth/registration", singUp);
+router.get("/users/all", getAllUsers);
 
 export default router;
